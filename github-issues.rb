47299414f4a76ce4edb91a1f75b710fed1bd9d11
i2bskn <i2bskn@gmail.com>
2015-11-28 02:17:27 +0900

    update

--- github-issues.rb
+++ github-issues.rb
@@ -1,13 +1,24 @@
 require "formula"
 
 class GithubIssues < Formula
+  desc "List of GitHub issues"
   homepage "https://github.com/i2bskn/github-issues"
-  version "v0.0.2"
   url "https://github.com/i2bskn/github-issues/releases/download/v0.0.2/github-issues_darwin_amd64.tar.gz"
-  sha1 "356832476d664448a2a5eec4a64c2e1d7da786c6"
-  depends_on :arch => :intel
+  sha256 "af7018a172d7fb96a8864c675e3888170f6bdc0d50966736846761d6150847ed"
+  version "v0.0.2"
 
   def install
     bin.install "github-issues"
+  end
+
+  def caveats; <<-EOS.undent
+    github-issues is required personal access token of GitHub.
+    Execute following command to set personal access token.
+
+    git config --global github.token "Your personal access token"
+
+    How to make personal access token:
+    https://help.github.com/articles/creating-an-access-token-for-command-line-use/
+    EOS
   end
 end
